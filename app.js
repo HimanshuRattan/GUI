83218d4ad30dd0454f4d21c0feb18686a53fe46b
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2018-08-24 21:02:49 +0530

    lol

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 1280, height: 800})
+    win = new BrowserWindow({width: 1360, height: 760})
   
     // and load the index.html of the app.
     win.loadFile('index.html')
@@ -10,7 +10,21 @@ const {app, BrowserWindow} = require('electron')
   
   app.on('ready', createWindow)
 
+
+
+function getInput(){
+  var w=document.getElementById("lat1").value;
+  var x=document.getElementById("long1").value;
+  var y=document.getElementById("lat2").value;
+  var z=document.getElementById("long2").value;
+ // document.getElementById("item4").innerHTML = "Value of z="+z;
+
+}
+  
+
+
 function myFunction() {
+
   var map = L.map('map').setView([12.818825299999999, 80.0478493], 13);
   // improve experience on mobile
   if (map.tap) map.tap.disable();
@@ -24,11 +38,11 @@ function myFunction() {
 // path to the icon
       iconUrl: 'icons/marker.png',
 // size of the icon 
-     iconSize:     [60, 52],
+     iconSize:     [52, 60],
 // point of the icon which will correspond to marker's location 
-      iconAnchor:   [0, 26],
+      iconAnchor:   [26, 60],
 // point of the icon where the popup window will open
-      popupAnchor: [35, -26]
+      popupAnchor: [0, -60]
   });
 
   var rp = L.icon({
@@ -38,15 +52,26 @@ function myFunction() {
       popupAnchor: [0, -60]
   });
 
+  //var a=w;
+  //var b=x;
+  //var c=y;
+  //var d=z;
 
   var p1 = L.marker([12.838581915230744, 80.04501342773439], {icon: srf}).addTo(map);
   var p2 = L.marker([12.825191972118786, 80.02578735351564], {icon: rp}).addTo(map);
-
-
-  p1.bindPopup("<strong style='color: #84b819'>Location 1");
  
+  
+  /*var p3 = L.marker([w, x], {icon: rp}).addTo(map);
+  */p1.bindPopup("<strong style='color: #84b819'>Location 1").openPopup();
+  p2.bindPopup("<strong style='color: #84b819'> Location 2");
+
+  /*var w=document.getElementById("lat1").value;
+  var x=document.getElementById("long1").value;*/
+
 // the .openPopup() opens this popup when the page is loaded
-p2.bindPopup("<strong style='color: #84b819'>Location 2").openPopup();
- 
+
+/*p3.bindPopup("<strong style='color: #84b819'> Start");
+p4.bindPopup("<strong style='color: #84b819'> End");
+ */
 var popup = L.popup();
 }
